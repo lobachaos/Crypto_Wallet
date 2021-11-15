574de4473b0149d78c00afa8ffd6948b19391825
lobachaos <zwolfsasa@gmail.com>
2021-11-15 12:23:10 -0400

    Config Cookies for Locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    if params[:locale] != nil
+      cookies[:locale] = params[:locale]
+    end
+
+    if cookies[:locale]
+      if I18n.locale != cookies[:locale]
+        I18n.locale = cookies[:locale]
+      end
+    end
+
+  end
 end
